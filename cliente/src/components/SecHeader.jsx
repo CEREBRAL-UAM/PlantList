1ae689908296fe90fa79da6fe351a2d56c82ceae
Brandon <brandon.juarez@cua.uam.mx>
2025-07-01 16:33:26 -0600

    Header secundario estilizado

--- cliente/src/components/SecHeader.jsx
+++ cliente/src/components/SecHeader.jsx
@@ -14,11 +14,23 @@ export function SecHeader({ dir }) {
     cargarDatosUsuario();
   }, []);
 
+  const imagenUrl = usuario.foto
+    ? `${usuario.foto}`
+    : "http://localhost:8000/media/fotos_perfil_usuarios/default.jpg";
+
   return (
-    <div className="w-full bg-amber-700">
+    <div className="w-1/2">
       <BotonAtras dir={dir} /> <br />
-      {usuario.Nombre}
-      {usuario.ApellidoPaterno} {usuario.ApellidoMaterno}
+      <div className="flex items-center gap-3 pl-8">
+        <img
+          src={imagenUrl}
+          alt="Imagen ni disponible"
+          className="rounded-full w-9 h-9"
+        />
+        <h2 className="dark:text-pl_white_a font-baloo">
+          {usuario.Nombre} {usuario.ApellidoPaterno}
+        </h2>
+      </div>
     </div>
   );
 }
--- cliente/src/components/botones/BotonAtras.jsx
+++ cliente/src/components/botones/BotonAtras.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import { ArrowLeft } from "lucide-react";
 
 export function BotonAtras({ dir }) {
   const navigate = useNavigate();
@@ -9,9 +10,14 @@ export function BotonAtras({ dir }) {
         console.log("clic");
         navigate(dir);
       }}
-      className="bg-amber-300"
     >
-      Atras
+      <ArrowLeft
+        className="
+        w-10 h-10 
+        cursor-pointer
+        pl-3
+        dark:text-pl_white_a"
+      />
     </button>
   );
 }
