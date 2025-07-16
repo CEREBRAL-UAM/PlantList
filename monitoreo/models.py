69e61a701dfd7abe7ad47dddc5dfd679ab773b55
mdaniela <mdanielasf6@gmail.com>
2025-07-16 17:13:33 -0600

    cambios app monitoreo

--- monitoreo/models.py
+++ monitoreo/models.py
@@ -18,3 +18,37 @@ class SensadoAmbiental(models.Model):
 
     def __str__(self):
         return f"Sensado {self.FechaSensado}"
+
+class sensadoSuelo(models.Model):
+    id_EnergiaPlanta = models.AutoField(primary_key=True)
+    id_Circuito = models.IntegerField()
+    fechaSensado = models.DateTimeField()
+    Voltaje = models.FloatField()
+    Amperaje = models.FloatField()
+    id_Electrodos = models.IntegerField()
+    id_Suelo = models.IntegerField(null=True, blank=True)
+    PhSuelo = models.CharField(max_length=45, null=True, blank=True)
+    HumedadSuelo = models.FloatField(null=True, blank=True)
+    id_PlantaIndividuo = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'sensadoSuelo'
+
+    def __str__(self):
+        return f"Sensado Suelo {self.fechaSensado}"
+
+class SensadoContaminantes(models.Model):
+    id_Circuito = models.IntegerField()
+    fechaSensado = models.DateTimeField(primary_key=True)
+    CO = models.DecimalField(max_digits=5, decimal_places=2)
+    CO2 = models.DecimalField(max_digits=5, decimal_places=2)
+    O = models.DecimalField(max_digits=5, decimal_places=2)
+    COVs = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table = 'sensadocontaminantes'
+        managed = False
+
+    def __str__(self):
+        return f"Contaminantes - {self.fechaSensado}"
--- monitoreo/serializers.py
+++ monitoreo/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from .models import SensadoAmbiental
+from .models import SensadoAmbiental, sensadoSuelo, SensadoContaminantes
+
 
 class SensadoAmbientalSerializer(serializers.ModelSerializer):
     class Meta:
         model = SensadoAmbiental
         fields = ['FechaSensado', 'TempAmbiental', 'Humedad', 'Lux', 'Radiacion']
+
+class SensadoSueloSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sensadoSuelo
+        fields = ['id_EnergiaPlanta', 'fechaSensado', 'Voltaje', 'Amperaje', 'PhSuelo', 'HumedadSuelo']
+
+class SensadoContaminantesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SensadoContaminantes
+        fields = ['fechaSensado', 'CO', 'CO2', 'O', 'COVs']
