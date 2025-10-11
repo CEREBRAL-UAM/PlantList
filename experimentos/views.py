5d7fff9b129c9b94384446d6c253dc4dcf2ff32a
Alberto <aslberto.chavez@cua.uam.mx>
2025-10-10 18:15:45 -0600

    Back para filtro de experimentos

--- experimentos/views.py
+++ experimentos/views.py
@@ -1,39 +1,44 @@
-from rest_framework import viewsets
 from .models import *
 from .serializers import *
+from .base import ModelViewSet, APIView
+from rest_framework.response import Response
 
-class TipoEstimulacionView(viewsets.ModelViewSet):
+class TipoEstimulacionView(ModelViewSet):
     serializer_class = TipoEstimulacionSerializer
     queryset = TipoEstimulacion.objects.all()
 
-class MaterialView(viewsets.ModelViewSet):
+class MaterialView(ModelViewSet):
     serializer_class = MaterialSerializer
     queryset = Material.objects.all()
 
-class ElectrodosView(viewsets.ModelViewSet):
+class ElectrodosView(ModelViewSet):
     serializer_class = ElectrodosSerializer
     queryset = Electrodos.objects.all()
 
-class UbicacionesView(viewsets.ModelViewSet):
+class UbicacionesView(ModelViewSet):
     serializer_class = UbicacionesSerializer
     queryset = Ubicaciones.objects.all()
 
-class SueloView(viewsets.ModelViewSet):
+class SueloView(ModelViewSet):
     serializer_class = SueloSerializer
     queryset = Suelo.objects.all()
 
-class EtapaDesarrolloView(viewsets.ModelViewSet):
+class EtapaDesarrolloView(ModelViewSet):
     serializer_class = EtapaDesarrolloSerializer
     queryset = EtapaDesarrollo.objects.all()
 
-class OrigenCrianzaView(viewsets.ModelViewSet):
+class OrigenCrianzaView(ModelViewSet):
     serializer_class = OrigenCrianzaSerializer
     queryset = OrigenCrianza.objects.all()
 
-class PlagasView(viewsets.ModelViewSet):
+class PlagasView(ModelViewSet):
     serializer_class = PlagasSerializer
     queryset = Plagas.objects.all()
 
-class PlantaIndividuoView(viewsets.ModelViewSet):
+class PlantaIndividuoView(ModelViewSet):
     serializer_class = PlantaIndividuoSerializer
     queryset = PlantaIndividuo.objects.all()
+
+class ExperimentosListadoView(APIView):
+    def get(self, request):
+        return Response({"Solo roles autorizados ven esto."})
