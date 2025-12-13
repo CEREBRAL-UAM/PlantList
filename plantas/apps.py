fabd7714c15aa5fc4552a1b68c4f0360d2a226c9
Brand2193 <tecnicobrandonjr@gmail.com>
2025-12-13 00:20:48 -0600

    Backend acoplado y gitignore actualizado

--- plantas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class PlantasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'plantas'
-
-    def ready(self): 
-        import plantas.signals
--- usuarios/permisos.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class Permisos(BasePermission):
-    def has_permission(self, request, view):
-        user = getattr(request, 'user', None)
-        if not user:
-            return False
-        tipo = getattr(user, 'TipoUsuario', None)
-        return tipo in ('isAdmin', 'isParticipant')
